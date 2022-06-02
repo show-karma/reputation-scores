c7c990895194c46a02aff550a7e346542d2e2294
Arthur <32548365+Arthh@users.noreply.github.com>
2022-06-02 08:12:00 -0300

    features: optimism score provider

---
+++ dist/score/optimism-dao-score.provider.d.ts
@@ -0,0 +1,8 @@
+import { BaseProvider, DelegateStat, GetDaoScore } from "./interfaces";
+export declare class OptimismDaoScoreProvider
+  extends BaseProvider
+  implements GetDaoScore
+{
+  getForumScore(stat: Partial<DelegateStat>): number;
+  getKarmaScore(stat: Partial<DelegateStat>, median: number): number;
+}
---
+++ dist/score/optimism-dao-score.provider.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OptimismDaoScoreProvider = void 0;
+const interfaces_1 = require("./interfaces");
+class OptimismDaoScoreProvider extends interfaces_1.BaseProvider {
+  getForumScore(stat) {
+    return (
+      Math.round(
+        stat.proposalsInitiated * 10 +
+          stat.proposalsDiscussed * 2 +
+          stat.forumPostCount +
+          stat.forumTopicCount * 3 +
+          stat.forumLikesReceived * 0.5 +
+          stat.forumPostsReadCount * 0.1
+      ) || 0
+    );
+  }
+  getKarmaScore(stat, median) {
+    return (
+      Math.round(
+        stat.forumActivityScore +
+          (stat.offChainVotesPct || 0) * 3 +
+          (stat.onChainVotesPct || 0) * 5
+      ) || 0
+    );
+  }
+}
+exports.OptimismDaoScoreProvider = OptimismDaoScoreProvider;
