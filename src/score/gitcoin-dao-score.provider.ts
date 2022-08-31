f8ea8eacff1cb5619de69128e9b241554ddba424
Arthur <32548365+Arthh@users.noreply.github.com>
2022-08-31 08:08:10 -0300

    fix: gitcoin discordMessagesCount to getKarmaScore

--- src/score/gitcoin-dao-score.provider.ts
+++ src/score/gitcoin-dao-score.provider.ts
@@ -4,7 +4,11 @@ import { DelegateStat } from "./interfaces";
 export class GitcoinDaoScoreProvider extends DefaultDaoScoreProvider {
   getKarmaScore(stat: Partial<DelegateStat>): number {
     return (
-      Math.round(stat.forumActivityScore + (stat.offChainVotesPct || 0)) || 0
+      Math.round(
+        stat.forumActivityScore +
+          (stat.offChainVotesPct || 0) +
+          (stat.discordMessagesCount || 0) * 0.01
+      ) || 0
     );
   }
 
@@ -16,8 +20,7 @@ export class GitcoinDaoScoreProvider extends DefaultDaoScoreProvider {
           stat.forumPostCount +
           stat.forumTopicCount * 3 +
           stat.forumLikesReceived * 0.5 +
-          stat.forumPostsReadCount * 0.1 +
-          (stat.discordMessagesCount || 0) * 0.01
+          stat.forumPostsReadCount * 0.1
       ) || 0
     );
   }
