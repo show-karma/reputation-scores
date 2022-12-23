59a6d1d39014acc440c81d3e5fdf51571fb93f56
Danylo Shapoval <danylo.shapoval@softonix.org>
2022-12-23 17:24:19 +0200

    gitcoin percentile separate provider score

---
+++ src/score/gitcoin-dao-percentile-score-provider.ts
@@ -0,0 +1,28 @@
+import {DefaultDaoScoreProvider} from "./default-dao-score.provider";
+import {DelegateStat} from "./interfaces";
+
+export class GitcoinDaoPercentileScoreProvider extends DefaultDaoScoreProvider {
+  getKarmaScore(stat: Partial<DelegateStat>): number {
+    return (
+      Math.round(
+        stat.forumActivityScore +
+        (stat.offChainVotesPct || 0) +
+        (stat.discordMessagePercentile || 0) * 0.01
+      ) || 0
+    );
+  }
+
+  // 1660 sum of all forum props percentiles
+  getForumScore(stat: Partial<DelegateStat>): number {
+    return (
+      Math.round(
+        ((stat.proposalsInitiatedPercentile || 0) * 10 +
+          (stat.proposalsDiscussedPercentile || 0) * 2 +
+          (stat.forumPostCountPercentile || 0) +
+          (stat.forumTopicCountPercentile || 0) * 3 +
+          (stat.forumLikesReceivedPercentile || 0) * 0.5 +
+          (stat.forumPostsReadCountPercentile || 0) * 0.1) * 100 / 1660
+      ) || 0
+    );
+  }
+}
