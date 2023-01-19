2f75ee74c524cf303808951d42a27ad54c0a9b94
mury12 <jamesandre79@hotmail.com>
2023-01-19 17:09:35 -0300

    updating build

--- dist/score/ens-dao-score.provider.d.ts
+++ dist/score/ens-dao-score.provider.d.ts
@@ -1,8 +1,7 @@
 import { DefaultDaoScoreProvider } from "./default-dao-score.provider";
-import { DelegateStat, DelegateStatPeriod, ScoreBreakdownCalc } from "./interfaces";
+import { DelegateStat } from "./interfaces";
 export declare class EnsDaoScoreProvider extends DefaultDaoScoreProvider {
     getForumScore(stat: Partial<DelegateStat>): number;
     getKarmaScore(stat: Partial<DelegateStat>, median: number): number;
     getKarmaScoreProps(): (keyof Partial<DelegateStat> | "median")[];
-    getScoreBreakdownCalc(stat: Partial<DelegateStat>, period?: DelegateStatPeriod, type?: "forum" | "score"): ScoreBreakdownCalc;
 }
