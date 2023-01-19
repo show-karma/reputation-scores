957c9f03ee5b7b29c720e49b5c085afb9bdc2568
mury12 <jamesandre79@hotmail.com>
2023-01-19 16:58:55 -0300

    fixed ens provider

--- src/score/ens-dao-score.provider.ts
+++ src/score/ens-dao-score.provider.ts
@@ -1,3 +1,4 @@
+import { coalesce } from "src/util/get-weights";
 import { DefaultDaoScoreProvider } from "./default-dao-score.provider";
 import {
   DelegateStat,
@@ -7,25 +8,35 @@ import {
 
 export class EnsDaoScoreProvider extends DefaultDaoScoreProvider {
   getForumScore(stat: Partial<DelegateStat>): number {
+    const {
+      forumScore: { lifetime = {} },
+    } = this.weights;
     return (
       Math.round(
-        stat.proposalsInitiated * 10 +
-          stat.proposalsDiscussed * 2 +
-          stat.forumPostCount +
-          stat.forumTopicCount * 3 +
-          stat.forumLikesReceived * 0.5 +
-          stat.forumPostsReadCount * 0.1
+        stat.proposalsInitiated * coalesce(lifetime.proposalsInitiated, 1) +
+          stat.proposalsDiscussed * coalesce(lifetime.proposalsDiscussed, 1) +
+          stat.forumPostCount * coalesce(lifetime.forumPostCount, 1) +
+          stat.forumTopicCount * coalesce(lifetime.forumTopicCount, 1) +
+          stat.forumLikesReceived * coalesce(lifetime.forumLikesReceived, 1) +
+          stat.forumPostsReadCount * coalesce(lifetime.forumPostsReadCount, 1)
       ) || 0
     );
   }
 
   getKarmaScore(stat: Partial<DelegateStat>, median: number): number {
+    const {
+      score: { lifetime = {} },
+    } = this.weights;
     return (
       Math.round(
-        (stat.offChainVotesPct || 0) * 5 +
-          (stat.onChainVotesPct || 0) * 2 +
-          stat.delegatedVotes / 1000 +
-          (stat.discordMessagesCount || 0) * 0.01
+        stat.forumActivityScore ||
+          0 * coalesce(lifetime.forumActivityScore, 1) +
+            (stat.offChainVotesPct || 0) *
+              coalesce(lifetime.offChainVotesPct, 1) +
+            (stat.onChainVotesPct || 0) *
+              coalesce(lifetime.onChainVotesPct, 1) +
+            (stat.discordMessagesCount || 0) *
+              coalesce(lifetime.discordMessagesCount, 1)
       ) || 0
     );
   }
@@ -37,15 +48,5 @@ export class EnsDaoScoreProvider extends DefaultDaoScoreProvider {
       "delegatedVotes",
       "discordMessagesCount",
     ];
-  }
-
-  getScoreBreakdownCalc(
-    stat: Partial<DelegateStat>,
-    period?: DelegateStatPeriod,
-    type?: "forum" | "score"
-  ): ScoreBreakdownCalc {
-    if (type === "forum") return [];
-
-    return [];
   }
 }
