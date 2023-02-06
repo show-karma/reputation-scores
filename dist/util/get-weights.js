539c860b7dfc001b303451685f5be2c419b8e0ea
mury12 <jamesandre79@hotmail.com>
2023-02-06 14:40:33 -0300

    fix: 404 get-weights

--- dist/util/get-weights.js
+++ dist/util/get-weights.js
@@ -1,10 +1,30 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.coalesce = exports.getTotalWeight = exports.getWeights = void 0;
-const axios_1 = __importDefault(require("axios"));
+const axios_1 = __importStar(require("axios"));
 const githubUrl = (resourceName) => `https://raw.githubusercontent.com/show-karma/reputation-scores/main/src/util/weights/${resourceName}.json`;
 const weights = {};
 async function getWeights(resourceName) {
@@ -15,8 +35,7 @@ async function getWeights(resourceName) {
         return resource;
     }
     catch (err) {
-        const error = err;
-        if (error.response.status === 404) {
+        if (err instanceof axios_1.AxiosError && err.response?.status === 404) {
             return getWeights("default");
         }
     }
