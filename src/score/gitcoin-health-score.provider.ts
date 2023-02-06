c3ccb39501bb0162ea6aecc85a80f0a1b954da85
mury12 <jamesandre79@hotmail.com>
2023-02-06 10:45:45 -0300

    feat: steward days

--- src/score/gitcoin-health-score.provider.ts
+++ src/score/gitcoin-health-score.provider.ts
@@ -271,7 +271,7 @@ export class GitcoinHealthScoreProvider implements AdditionalScoreProvider {
               },
               {
                 label: `Square root of Steward Days`,
-                value: Math.min(this.getStewardDays(publicAddress)),
+                value: this.getStewardDays(publicAddress),
                 weight: 1,
                 op: "/",
               },
