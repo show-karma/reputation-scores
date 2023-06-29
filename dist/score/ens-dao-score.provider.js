875086c1e271ca2e29af0be16be49f67fe0bd91a
Arthh <arthur.ulhoa28@gmail.com>
2023-06-29 15:01:51 -0300

    hotfix: ens provider

--- dist/score/ens-dao-score.provider.js
+++ dist/score/ens-dao-score.provider.js
@@ -2,12 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.EnsDaoScoreProvider = void 0;
 const get_weights_1 = require("../util/get-weights");
-const default_dao_score_provider_1 = require("./default-dao-score.provider");
-class EnsDaoScoreProvider extends default_dao_score_provider_1.DefaultDaoScoreProvider {
+const interfaces_1 = require("./interfaces");
+class EnsDaoScoreProvider extends interfaces_1.BaseProvider {
+    constructor(resourceName) {
+        super(resourceName);
+        this.resourceName = resourceName;
+    }
+    async preload(resourceName) {
+        this.weights = await (0, get_weights_1.getWeights)(resourceName || this.resourceName || "ens");
+    }
     getForumScore(stat) {
         const { forumScore: { lifetime = {} }, } = this.weights;
         const totalWeight = (0, get_weights_1.getTotalWeight)(lifetime);
-        return Math.round((((0, get_weights_1.coalesce)(stat.proposalsInitiatedPercentile, 0) *
+        return Math.round((((0, get_weights_1.coalesce)(stat.proposalsInitiated, 0) *
             (0, get_weights_1.coalesce)(lifetime.proposalsInitiated, 1) +
             (0, get_weights_1.coalesce)(stat.proposalsDiscussed, 0) *
                 (0, get_weights_1.coalesce)(lifetime.proposalsDiscussed, 1) +
@@ -38,10 +45,106 @@ class EnsDaoScoreProvider extends default_dao_score_provider_1.DefaultDaoScorePr
     }
     getKarmaScoreProps() {
         return [
+            "forumActivityScore",
             "offChainVotesPct",
             "onChainVotesPct",
-            "delegatedVotes",
             "discordMessagesCount",
+        ];
+    }
+    getScoreBreakdownCalc(stat, period, type = "score") {
+        const { score: { lifetime: score }, forumScore: { lifetime: forum }, } = this.weights;
+        if (type == "forum")
+            return [
+                {
+                    label: "Max Score Setting",
+                    value: 100,
+                    weight: 1,
+                    childrenOp: "*",
+                    children: [
+                        {
+                            label: "Proposals Initiated",
+                            value: (0, get_weights_1.coalesce)(stat.proposalsInitiated),
+                            weight: (0, get_weights_1.coalesce)(forum.proposalsInitiated, 1),
+                            op: "*",
+                        },
+                        {
+                            label: "Proposals Discussed",
+                            value: (0, get_weights_1.coalesce)(stat.proposalsDiscussed),
+                            weight: (0, get_weights_1.coalesce)(forum.proposalsDiscussed, 1),
+                            op: "+",
+                        },
+                        {
+                            label: "Forum Post Count",
+                            value: (0, get_weights_1.coalesce)(stat.forumPostCount),
+                            weight: (0, get_weights_1.coalesce)(forum.forumPostCount, 1),
+                            op: "+",
+                        },
+                        {
+                            label: "Forum Topic Count ",
+                            value: (0, get_weights_1.coalesce)(stat.forumTopicCount),
+                            weight: (0, get_weights_1.coalesce)(forum.forumTopicCount, 1),
+                            op: "+",
+                        },
+                        {
+                            label: "Forum Likes Received",
+                            value: (0, get_weights_1.coalesce)(stat.forumLikesReceived),
+                            weight: (0, get_weights_1.coalesce)(forum.forumLikesReceived, 1),
+                            op: "+",
+                        },
+                        {
+                            label: "Forum Posts Read Count",
+                            value: (0, get_weights_1.coalesce)(stat.forumPostsReadCount),
+                            weight: (0, get_weights_1.coalesce)(forum.forumPostsReadCount, 1),
+                            op: "+",
+                        },
+                    ],
+                },
+                {
+                    label: "Sum of Weights times Max Score Setting",
+                    value: (0, get_weights_1.getTotalWeight)(forum),
+                    weight: 1,
+                    op: "/",
+                },
+            ];
+        return [
+            {
+                label: "Max Score Setting",
+                value: 100,
+                weight: 1,
+                childrenOp: "*",
+                children: [
+                    {
+                        label: "Forum Activity Score",
+                        value: (0, get_weights_1.coalesce)(stat.forumActivityScore),
+                        weight: (0, get_weights_1.coalesce)(score.forumActivityScore, 1),
+                        op: "*",
+                    },
+                    {
+                        label: "Off-chain Votes %",
+                        value: (0, get_weights_1.coalesce)(stat.offChainVotesPct),
+                        weight: (0, get_weights_1.coalesce)(score.offChainVotesPct, 1),
+                        op: "+",
+                    },
+                    {
+                        label: "On-chain Votes %",
+                        value: (0, get_weights_1.coalesce)(stat.onChainVotesPct),
+                        weight: (0, get_weights_1.coalesce)(score.onChainVotesPct, 1),
+                        op: "+",
+                    },
+                    {
+                        label: "Discord Messages %",
+                        value: (0, get_weights_1.coalesce)(stat.discordMessagesCount),
+                        weight: (0, get_weights_1.coalesce)(score.discordMessagesCount, 1),
+                        op: "+",
+                    },
+                ],
+            },
+            {
+                label: "Sum of Weights times Max Score Setting",
+                value: (0, get_weights_1.getTotalWeight)(score),
+                weight: 1,
+                op: "/",
+            },
         ];
     }
 }
