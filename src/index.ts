983b5a13532d6abae20becaed6ea7b1404c280b7
Arthh <arthur.ulhoa28@gmail.com>
2023-09-29 13:58:41 -0300

    feat: export DefaultWithDiscordDaoPercentileScoreProvider

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,4 @@ export * from "./score/apecoin-dao-percentile-score.provider";
 export * from "./score/giveth-percentile-score.provider";
 export * from "./score/ssvnetwork-dao-percentile-score.provider";
 export * from "./score/moonbeam-percentile-score.provider";
+export * from "./score/default-dao-with-discord-percentile-score.provider";
